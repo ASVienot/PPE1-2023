d365805b7cd829b8658ab2397d49cd3cd0a48eb8
Alix S-Vienot <alix.vienot@gmail.com>
2023-10-25 12:02:00 +0200

    fin de cours 25

---
+++ exos/test_exo.sh
@@ -0,0 +1,3 @@
+#usr/bin/bash
+
+echo "on a fini"
--- mini-projet/programmes/miniprojet.sh
+++ mini-projet/programmes/miniprojet.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
+urlfile=$1
 
 if [ $# -ne 1 ]
-then
+do:
+	echo "un argument attendu exactement"
 	exit
+done:
+
+
+if [-f $urlfile]
+	then
+		echo ""
+	else
+		echo "on attend un fichier qui existe"
+		exit
 fi
-#vérifie qu'on a un argument
+#vérifie qu'on a un seul argument et que le fichier existe
 
 num_ligne=1
 
 while read -r line;
 do
-	echo -e "$num_ligne\t$line";
+	status=$(curl -i -s $line |egrep "^HTTP"| head -n 1)
+	#trouve les infos qui commence par HTTP et prend la premiere ligne
+	echo -e "$num_ligne\t$line\t$status";
+	# affiche avec une tab entre le num_ligne l'URL et le status de l'url
 	num_ligne=$(expr $num_ligne + 1)
 	#le séparateur par tab -e \t
 done < "urls/fr.txt"
-#comment transformer ligne du dessus en parmètre
